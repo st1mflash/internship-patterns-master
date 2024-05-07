60f84533d90964b2b7835339317bc9d51b296a5c
ansekolesnikov <st1mflash@gmail.com>
2024-05-07 17:13:55 +0300

    Refactor ManagerRole.java

--- src/main/java/ru/liga/internshippatterns/model/role/ManagerRole.java
+++ src/main/java/ru/liga/internshippatterns/model/role/ManagerRole.java
@@ -4,12 +4,11 @@ import ru.liga.internshippatterns.model.Application;
 import ru.liga.internshippatterns.model.ApplicationStatus;
 
 public interface ManagerRole {
-    default public void review(Application application) {
+    default void review(Application application) {
         application.setStatus(ApplicationStatus.PROCESSING);
     }
-
-    default public void approve(Application application) {
+    default void approve(Application application) {
         application.setStatus(ApplicationStatus.APPROVED);
     }
-    public void sign(Application application);
+    void sign(Application application);
 }
